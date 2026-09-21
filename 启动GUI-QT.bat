d794ec40a0b7aafe2dc1f9e8f990881d2ac9f564
vic4728 <150593698+vic4728@users.noreply.github.com>
2026-09-22 03:01:02 +0800

    docs: README 与本地启动脚本

---
+++ 启动GUI-QT.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+chcp 65001 >nul
+rem 爱快路由-AP终端工具 PySide6 版启动器
+set PY=C://Users//VIC-AMD//.workbuddy//binaries//python//envs//ikuai_gui//Scripts//pythonw.exe
+if not exist "%PY%" set PY=%~dp0.venv-gui\Scripts\pythonw.exe
+start "" "%PY%" "%~dp0ikuai_gui_qt.py"
